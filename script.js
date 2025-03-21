9928c12f49bfee4c40252374d074250091b57016
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-21 23:59:17 +1100

    Add basic printing of record collection

--- script.js
+++ script.js
@@ -12,5 +12,19 @@ class collection {
     constructor() {
         this.records = [];
     }
+
+    addVinyl(vinyl) {
+        this.records.push(vinyl);
+    }
+
+    display() {
+        console.log(this.records);
+    }
 }
 
+const newVinyl = new vinyl("After Hours", "The Weeknd", "R&B", "2020", "Good");
+const myCollection = new collection();
+
+myCollection.addVinyl(newVinyl);
+
+myCollection.display();

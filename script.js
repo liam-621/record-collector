d5b5ca38189f80697cafb57121c28ebff25129f5
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-22 00:46:50 +1100

    Create add vinyl button

--- script.js
+++ script.js
@@ -18,18 +18,29 @@ class collection {
     }
 }
 
-const newVinyl = new vinyl("After Hours", "The Weeknd", "R&B", "2020", "Good");
+// let newVinyl = new vinyl("After Hours", "The Weeknd", "R&B", "2020", "Good");
 const myCollection = new collection();
 
-myCollection.addVinyl(newVinyl);
+// myCollection.addVinyl(newVinyl);
 
-// Convert collection to a string for outputting
-const collectionString = myCollection.records.map(vinyl => `${vinyl.title} by ${vinyl.artist}`).join("\n");
+const addBtn = document.querySelector("#addVinyl"); // Prompt user for vinyl information
+addBtn.addEventListener("click", function() {
+    let title = prompt("Title?");
+    let artist = prompt("Artist?");
+    let genre = prompt("Genre?");
+    let releaseYear = prompt("Release Year?")
+    let condition = prompt("Condition?");
+    let newVinyl = new vinyl(title, artist, genre, releaseYear, condition);
+    myCollection.addVinyl(newVinyl);
+    console.log(myCollection.records);
+});
 
-const btn = document.querySelector("button");
+const printBtn = document.querySelector("#printCollection");
 
-btn.addEventListener("click", function() { // Listener for button which will print collection
-    document.querySelector("p").textContent = collectionString;
+printBtn.addEventListener("click", function() { // Listener for button which will print collection
+    // Convert collection to a string for outputting
+    const collectionString = myCollection.records.map(vinyl => `${vinyl.title} by ${vinyl.artist}`).join("\n"); 
+    document.querySelector("#collection").textContent = collectionString;
 });
 
 

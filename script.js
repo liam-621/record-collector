c5b3bb97b7e6d4267f009107f4eb3c412dcd49e2
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-21 23:27:48 +1100

    Create initial html and script with vinyl class

---
+++ script.js
@@ -0,0 +1,9 @@
+class vinyl {
+    constructor(title, artist, genre, releaseYear, condition) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.condition = condition;
+    }
+}
